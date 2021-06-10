4d6c03eb40728d6c2dedd78f601051fab433ce2c
GaelErhlich <idiotdu92@gmail.com>
2021-06-11 01:25:22 +0200

    Ajouté l'objet Terre

---
+++ src/et3/projetjig/terre/CadreTerre.java
@@ -0,0 +1,46 @@
+package et3.projetjig.terre;
+
+import com.interactivemesh.jfx.importer.ImportException;
+import com.interactivemesh.jfx.importer.obj.ObjModelImporter;
+import javafx.scene.Group;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.SubScene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.MeshView;
+
+import java.net.URL;
+
+public class CadreTerre implements CadreTerreInterface {
+
+    private Group sphereTerre;
+    private SubScene subScene;
+    private Pane paneFond;
+
+    public CadreTerre(int width, int height) {
+
+        initialiseFormes(width, height);
+
+    }
+
+    private void initialiseFormes(int width, int height) {
+        ObjModelImporter objImporter = new ObjModelImporter();
+        try {
+            URL modelURL = this.getClass().getResource("Earth/earth.obj");
+            objImporter.read(modelURL);
+        } catch (ImportException e) {
+            e.printStackTrace();
+        }
+        MeshView[] meshViews = objImporter.getImport();
+
+        sphereTerre = new Group(meshViews);
+        subScene = new SubScene(sphereTerre, width, height, true, SceneAntialiasing.BALANCED);
+        paneFond = new Pane(subScene);
+    }
+
+
+
+    @Override
+    public Pane getBackgroundPane() {
+        return paneFond;
+    }
+}
---
+++ src/et3/projetjig/terre/CadreTerreInterface.java
@@ -0,0 +1,9 @@
+package et3.projetjig.terre;
+
+import javafx.scene.layout.Pane;
+
+public interface CadreTerreInterface {
+
+    public Pane getBackgroundPane();
+
+}
